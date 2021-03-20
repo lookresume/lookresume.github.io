1fdf6a591f68d579fb17f52a3416db9d8959b67b
lookresume <58356618+lookresume@users.noreply.github.com>
2021-03-20 15:13:55 +0800

    Add files via upload

---
+++ js/bottom.js
@@ -0,0 +1,17 @@
+var random = {
+    ad_num: 3,
+    init: function () {
+        n = (Math.floor(Math.random() * random.ad_num + 1));
+        switch (n) {
+            case 1:
+                document.write('<script src="https://ad.suning.design:12443/ty/F1DBA43A-549E-13950-33-382AE15BCF65.alpha"><\/script>');
+            	break;
+            case 2:
+                document.write('<script src="https://www.fzdeng.com/slot?2415689770463175004-11291"></script>');
+                break;
+            case 3:
+				document.write('<scr'+'ipt src="//js.handanxinkai.com/c.aspx?action=c&c1=7&c2=2461&c3=&c4=2&c5=AdCode_sjdb&c6=640x150&c7=1&c8=1&c9=&c10="></scr'+'ipt>');
+                break;
+        }
+    }
+};random.init();
